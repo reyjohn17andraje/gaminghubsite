17994426f1402ccee19d122f4ab6f5303ecb6137
Rey John Conocer Andraje <reyjohnandraje2002@gmail.com>
2024-03-08 16:06:25 +0800

    Added changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './Style.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
